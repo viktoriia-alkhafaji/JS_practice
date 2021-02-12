3989619a85ef523f1cbf046c26bab934032b1052
viktoriia-alkhafaji <78023870+viktoriia-alkhafaji@users.noreply.github.com>
2021-02-12 09:46:12 +0100

    added style for forms,
    improved serialize and style.js code

--- project_13_Getting_data_from_forms/js/form.js
+++ project_13_Getting_data_from_forms/js/form.js
@@ -1,15 +1,32 @@
+// get the value form select
 let select = document.querySelector('#select-1');
-
 select.onchange = function () {
     console.log(select.value);
 }
 
-let radioButtons = document.querySelectorAll('input[name=radio-1]');
-console.log(radioButtons.length);
-
-let sendForm = document.querySelector('#send-form');
-
-sendForm.onclick = (event)=> {
-    event.preventDefault();
+// get value from radio buttons
+let radioButtons = document.querySelectorAll('input[name="choice"]');
+let btn = document.querySelector('#btn');
+btn.onclick = () => {
     console.log('work');
+    for (let i = 0; i < radioButtons.length; i++) {
+        if (radioButtons[i].checked) {
+            console.log(radioButtons[i].value);
+        }
+    }
+} 
+
+let form1 = document.querySelector('#form-1');
+let sendForm1 = document.querySelector('#send-form-1');
+sendForm1.onclick = function (event) {
+    event.preventDefault();
+    console.log('button form 1 ');
+    console.log(serialize(form1));
+}
+
+let form = document.querySelector('#form');
+form.onsubmit = function (event) {
+    event.preventDefault();
+    console.log(222222);
+    console.log(serialize(form));
 }
